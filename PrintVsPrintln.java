f5d315b07fed4a7c591f8fe577395f12897242d2
tekemehmet <tekemehmet@yahoo.com>
2020-01-12 18:53:40 -0500

    Add files via upload

---
+++ PrintVsPrintln.java
@@ -0,0 +1,19 @@
+package day03;
+
+public class PrintVsPrintln {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\t World line1");
+        System.out.println("Hello\n World line2");
+        System.out.println("Hello\' World line3");
+        System.out.print("Hello\" World4");
+        System.out.println("Hello World5");
+        System.out.println("I like the book \"Java\" ");
+
+
+    }
+
+
+
+}
