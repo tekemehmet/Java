dd6f76046666b432389d748446c43b9fb6bc0d36
tekemehmet <tekemehmet@yahoo.com>
2020-01-12 18:53:02 -0500

    Add files via upload

---
+++ EscapeCharacter.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class EscapeCharacter {
+    public static void main(String[] args) {
+
+        System.out.println("My name is \tMehmet Teke"); // One tab offset
+        System.out.println("My name is \nMehmet Teke"); //Pass the new line
+        System.out.println("Mehmet\'in defteri");
+        System.out.println("My favorite programming language is \"JAVA\".");
+
+
+
+    }
+}
