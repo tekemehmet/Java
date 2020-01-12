c540b8bbfb042306b593ef3e8b3557329a7111bc
tekemehmet <tekemehmet@yahoo.com>
2020-01-12 18:52:21 -0500

    Add files via upload
    Character and Boolean

---
+++ CharacterAndBoolean.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class CharacterAndBoolean {
+
+    public static void main(String[] args) {
+
+        char grade = 'A';
+        char emailSign = '@';
+
+        System.out.println("My score for Math is " + grade);
+
+        System.out.println("E-mail sign is " + emailSign);
+
+        boolean isLightOn = true;
+        System.out.println("did you turn off the light?" + isLightOn);
+
+        boolean areYouSleeping = false;
+        System.out.println("are you sleeping?" + areYouSleeping);
+
+        boolean isWeatherNice = false;
+
+    }
+}
