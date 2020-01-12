4aa15d9120b33c3c5c305e525b9af82f3767e767
tekemehmet <tekemehmet@yahoo.com>
2020-01-12 18:54:01 -0500

    Add files via upload

---
+++ StringDataType.java
@@ -0,0 +1,27 @@
+package day03;
+
+public class StringDataType {
+
+    public static void main(String[] args) {
+
+        String name = "Mehmet" ;
+        String lastName = "Teke";
+        String placeOfbirth = "Turkey";
+        int age = 39;
+        boolean isMarried = true;
+        byte howManyKids = 2;
+        long howFarHomland = 1000000000L;
+
+
+        System.out.println("My name is: " + name);
+
+        System.out.println("My last name is " + lastName);
+        System.out.println("Are you married " + isMarried);
+        System.out.println("Where did you birn " + placeOfbirth);
+        System.out.println("Hor old are you " + age);
+        System.out.println("How many kids ? " + howManyKids);
+
+
+
+    }
+}
