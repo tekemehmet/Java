a6f501656847237d4fac4ba6bad9fc25600b3f08
tekemehmet <tekemehmet@yahoo.com>
2020-01-12 18:52:49 -0500

    Add files via upload

---
+++ Concenatation.java
@@ -0,0 +1,11 @@
+package day03;
+
+public class Concenatation {
+    public static void main(String[] args) {
+
+        String firstName = "Mehmet";
+        String lastName = "Teke";
+
+        System.out.println( firstName + " " + lastName ); // Bosluk icin quotation mark kullan
+    }
+}
