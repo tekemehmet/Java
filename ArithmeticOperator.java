3b0166fb21df36dc9e2df10482c616dcad1b0306
tekemehmet <tekemehmet@yahoo.com>
2020-01-13 16:26:44 -0500

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,23 @@
+package day04;
+
+public class ArithmeticOperator {
+
+    public static void main(String[] args) {
+
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/3);
+
+        System.out.println(5+3.0d);
+        System.out.println(5-3.0d);
+        System.out.println(5*3.0d);
+        System.out.println(5/3.0d);
+        System.out.println(5/3.0f);
+
+        double a = 5;
+        double b = 14.96d;
+        double result = a*b;
+        System.out.println(result);
+    }
+}
