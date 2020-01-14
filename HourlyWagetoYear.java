512f2b2090eb4bfd4e8510028b335b3178f92a00
tekemehmet <tekemehmet@yahoo.com>
2020-01-14 17:29:43 -0500

    Add files via upload

---
+++ HourlyWagetoYear.java
@@ -0,0 +1,11 @@
+package day05;
+
+import java.util.Scanner;
+
+public class HourlyWagetoYear {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
