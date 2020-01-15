0ab21788e4267ff9fb4c2f7d5055be7415ee9495
tekemehmet <tekemehmet@yahoo.com>
2020-01-15 17:37:08 -0500

    Add files via upload

---
+++ PreIncrementDecrementOperator.java
@@ -0,0 +1,22 @@
+package day06;
+
+public class PreIncrementDecrementOperator {
+
+    public static void main(String[] args) {
+
+        int offerCount = 2 ;
+
+        // offerCount = offerCount + 1 ;
+        // offerCount += 1;
+
+        //offerCount ++;
+        offerCount ++;
+        -- offerCount;
+
+        System.out.println("Offer Now " + offerCount);
+
+
+
+
+    }
+}
