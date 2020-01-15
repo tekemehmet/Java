c639b542db1b9fce1a960611f37e3eb32245fa5e
tekemehmet <tekemehmet@yahoo.com>
2020-01-15 17:37:19 -0500

    Add files via upload

---
+++ Remainder.java
@@ -0,0 +1,22 @@
+package day06;
+
+public class Remainder {
+
+    public static void main(String[] args) {
+
+        // + - * /
+
+        System.out.println(5/2);  // Result is 2  because of variable type is int
+
+        System.out.println(5.0/2); // Result 2.5  Variable type is double
+
+        System.out.println(5.0/2f);
+
+        //  % Remainder( Kalan degeri bulma)
+
+        System.out.println(5%2); // Result is 1, because remainder is 1
+        System.out.println(99%10);    // Remainder is 9
+        System.out.println(100%10);
+
+    }
+}
