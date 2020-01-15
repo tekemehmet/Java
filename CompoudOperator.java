8617003c44f7bfd3f396bc9a2c830940f2e70244
tekemehmet <tekemehmet@yahoo.com>
2020-01-15 17:35:48 -0500

    Add files via upload

---
+++ CompoudOperator.java
@@ -0,0 +1,40 @@
+package day06;
+
+public class CompoudOperator {
+
+    public static void main(String[] args) {
+
+        int studenOnline = 263 ;
+
+        studenOnline = studenOnline + 5 ;
+        System.out.println(studenOnline);
+
+        studenOnline = studenOnline - 5;
+        System.out.println(studenOnline);
+
+        studenOnline = studenOnline*2;
+        System.out.println(studenOnline);
+
+
+
+
+        // +=, -= , *= , /= , %=
+
+        studenOnline += 5;
+        System.out.println(studenOnline);
+
+        studenOnline -= 5;
+        System.out.println(studenOnline);
+
+        studenOnline *= 2;
+        System.out.println(studenOnline);
+
+        studenOnline /= 2;
+        System.out.println(studenOnline);
+
+        studenOnline %= 50;
+        System.out.println(studenOnline);
+
+
+    }
+}
