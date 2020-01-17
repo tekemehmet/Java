5c44b2573887bbfa89497b603971f28a3c9ef9ca
tekemehmet <tekemehmet@yahoo.com>
2020-01-17 18:56:44 -0500

    Add files via upload

---
+++ RelationalOperator_Variable.java
@@ -0,0 +1,25 @@
+package day07;
+
+public class RelationalOperator_Variable {
+
+    public static void main(String[] args) {
+
+        int myNumber = 10 ;
+
+        System.out.println(myNumber>5);
+        System.out.println(myNumber<-19);
+        System.out.println(myNumber>=5);
+        System.out.println(myNumber>=8);
+        System.out.println(myNumber<=5);
+        System.out.println(myNumber>=8);
+
+        // == is used to check for equality
+        // and very different from single =
+        System.out.println(myNumber==5);
+
+        // != is used to check for inequality
+        System.out.println(myNumber != 10);
+        System.out.println(myNumber != 6);
+
+    }
+}
