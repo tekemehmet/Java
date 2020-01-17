7c6aeb68f52886655f39850a38e53c0b560212bc
tekemehmet <tekemehmet@yahoo.com>
2020-01-17 18:56:23 -0500

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,37 @@
+package day07;
+
+public class RelationalOperator {
+
+    public static void main(String[] args) {
+
+        // > More than
+        // >= more than and equality
+        //< less than
+        // <= less than and equality
+        // == equality
+        // != not equality
+
+        //Relational operator is used to compare 2 oprands
+        // and return true or false
+        //anything action or expression
+        // can be called boolean operation
+
+        System.out.println(7>5);
+        System.out.println(7<-19);
+        System.out.println(5>=5);
+        System.out.println(7>=8);
+        System.out.println(4<=5);
+        System.out.println(17>=8);
+
+        // == is used to check for equality
+        // and very different from single =
+        System.out.println(5==5); //true
+
+        // != is used to check for inequality
+        System.out.println(5 != 10); //true
+        System.out.println(6 != 6); //false
+
+
+
+    }
+}
