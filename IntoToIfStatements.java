2b1d2324a2bd4bbcca056e53d410593bbd730794
tekemehmet <tekemehmet@yahoo.com>
2020-01-17 18:55:55 -0500

    Add files via upload

---
+++ IntoToIfStatements.java
@@ -0,0 +1,22 @@
+package day07;
+
+public class IntoToIfStatements {
+    public static void main(String[] args) {
+
+        int score = 70;
+        //  if (PUT YOUR CONDITION HERE)  {
+        //
+        // DO THIS IF CONDITION IS TRUE
+        //  }
+
+        if (score > 60) {
+
+            System.out.println(" You passed");
+
+        } else {
+            System.out.println(" The End ");
+
+
+        }
+    }
+}
