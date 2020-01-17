4ad94ac2708c37b63f6410b75012bb476ee66175
tekemehmet <tekemehmet@yahoo.com>
2020-01-17 18:55:41 -0500

    Add files via upload

---
+++ IncrementAndDecrement.java
@@ -0,0 +1,18 @@
+package day07;
+
+public class IncrementAndDecrement {
+
+    public static void main(String[] args) {
+
+        int apple = 10;
+
+        apple = apple +1;
+        apple +=1;
+        ++ apple ;
+        apple ++ ;
+
+
+
+        System.out.println("Apple" + ++apple);
+    }
+}
