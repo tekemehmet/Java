60088ff29b4de659909937228aa74501fb945405
tekemehmet <tekemehmet@yahoo.com>
2020-01-22 16:31:26 -0500

    Add files via upload

---
+++ LogicalOperator_SinglePipe.java
@@ -0,0 +1,11 @@
+package day011;
+
+public class LogicalOperator_SinglePipe {
+
+    public static void main(String[] args) {
+
+        //
+        System.out.println(10 > 5 || 9 / 0 == 3);
+        //System.out.println(10 > 5 |  9 / 0 == 3); //ERROR
+    }
+}
