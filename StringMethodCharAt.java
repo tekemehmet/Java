b1bcccc82b3c8d7e1fbddc94fb61a679e8259f84
tekemehmet <tekemehmet@yahoo.com>
2020-01-26 10:45:36 -0500

    Add files via upload

---
+++ StringMethodCharAt.java
@@ -0,0 +1,41 @@
+package day014;
+
+public class StringMethodCharAt {
+
+    public static void main(String[] args) {
+
+        // charAt method of String
+        // it will return a character at certain location specified
+
+
+        String aName = "Arya";
+        char c1 = aName.charAt(0);
+        char c2 = aName.charAt(1);
+        char c3 = aName.charAt(2);
+        char c4 = aName.charAt(3);
+
+        System.out.println(c1 + " "+ c2 + " " + c3 + " " + c4); // Concenatatoin olmazsa aritmetik toplama yapar
+
+
+
+        String name = "JON SNOW";
+
+        System.out.println(  name.charAt(0));
+        System.out.println(  name.charAt(1));
+        System.out.println(  name.charAt(2));
+        System.out.println(  name.charAt(3));
+        System.out.println(  name.charAt(4));
+        System.out.println(  name.charAt(5));
+        System.out.println(  name.charAt(6));
+        System.out.println(  name.charAt(7));
+       ;
+
+
+
+
+
+
+
+
+    }
+}
