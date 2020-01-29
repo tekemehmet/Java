f0b3c895cd844da32bdc11d73ff8798e6bd02de2
tekemehmet <tekemehmet@yahoo.com>
2020-01-28 19:02:16 -0500

    Add files via upload

---
+++ ReverseTheWord.java
@@ -0,0 +1,13 @@
+package day015;
+
+public class ReverseTheWord {
+
+    public static void main(String[] args) {
+
+        String movie = "Lord Of The Rings ";
+        System.out.println(movie.substring(12,16)+ " " + movie.substring(5,7) + " " + movie.substring(8,11)+ " " + movie.substring(0,4));
+
+
+
+    }
+}
