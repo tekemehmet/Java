e1125be131d5b4b418f4c68c005057290d98dea1
tekemehmet <tekemehmet@yahoo.com>
2020-01-29 20:13:26 -0500

    Add files via upload

---
+++ CountFrom10To1.java
@@ -0,0 +1,13 @@
+package day016;
+
+public class CountFrom10To1 {
+
+    public static void main(String[] args) {
+
+        int count = 10;
+
+        while ( count >= 1) {
+            System.out.println(count--);
+        }
+    }
+}
