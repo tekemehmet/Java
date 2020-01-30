a5700ea0e61f5b9bf3a089697f82969f9ac57905
tekemehmet <tekemehmet@yahoo.com>
2020-01-30 18:29:56 -0500

    Add files via upload

---
+++ WhileLoops.java
@@ -0,0 +1,20 @@
+package day016;
+
+
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+
+        int apples = 1;
+
+        while (apples <= 10) {
+
+            System.out.println("Enjoy it!!!");
+            apples++;
+        }
+
+        System.out.println("No more apples!!");
+    }
+}
