082028f0759dba8e1fb0da1a7217872f0ca139e6
tekemehmet <tekemehmet@yahoo.com>
2020-01-30 18:29:31 -0500

    Add files via upload

---
+++ WhileLoopIntro.java
@@ -0,0 +1,24 @@
+package day016;
+
+public class WhileLoopIntro {
+
+    public static void main(String[] args) {
+
+        //print "Hello World" 5 times
+        // also keep the count of how many times you said hello
+
+        int count = 1;
+
+        while (count <=5 ) {
+            System.out.println("Hello World");
+            System.out.println("count is : " + count);
+            ++count;
+        }
+
+
+
+
+
+
+    }
+}
