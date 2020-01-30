3579e907be01be49bec3ce09c026dbaf5b4d3855
tekemehmet <tekemehmet@yahoo.com>
2020-01-30 18:29:17 -0500

    Add files via upload

---
+++ WhileLoop_Alphabet.java
@@ -0,0 +1,14 @@
+package day016;
+
+public class WhileLoop_Alphabet {
+
+    public static void main(String[] args) {
+
+        char ch ='A';
+
+        while (ch <= 'Z') {
+            System.out.println(ch);
+            ++ch;
+        }
+    }
+}
