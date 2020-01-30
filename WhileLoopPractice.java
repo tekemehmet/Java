3519600d11497747840e238a8ffe69e6e83c53f9
tekemehmet <tekemehmet@yahoo.com>
2020-01-30 18:29:43 -0500

    Add files via upload

---
+++ WhileLoopPractice.java
@@ -0,0 +1,17 @@
+package day016;
+
+public class WhileLoopPractice {
+
+    public static void main(String[] args) {
+
+        int x = 100;
+
+        while ( x < 105 ){
+            System.out.println("Hello World");
+            ++x;
+        }
+
+
+
+    }
+}
