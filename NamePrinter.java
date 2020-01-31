b622840e1f63d0eca4e9be680baed746e76cdbb8
tekemehmet <tekemehmet@yahoo.com>
2020-01-31 17:41:42 -0500

    Add files via upload

---
+++ NamePrinter.java
@@ -0,0 +1,28 @@
+package day017;
+
+public class NamePrinter {
+
+    public static void main(String[] args) {
+
+        String name = "Alexander"; // 1-800-Alexander
+
+        // get each and every character and print them out vertically
+
+
+
+
+
+        System.out.println( name.charAt(0));
+        System.out.println( name.charAt(1));
+        System.out.println( name.charAt(2));
+        System.out.println( name.charAt(3));
+        System.out.println( name.charAt(4));
+        System.out.println( name.charAt(5));
+        System.out.println( name.charAt(6));
+        System.out.println( name.charAt(7));
+        System.out.println( name.charAt(8));
+
+
+
+    }
+}
