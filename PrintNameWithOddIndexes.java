ba5fbeb20ab8273e5171aad7aea9e5f48813397a
tekemehmet <tekemehmet@yahoo.com>
2020-02-01 10:33:57 -0500

    Add files via upload

---
+++ PrintNameWithOddIndexes.java
@@ -0,0 +1,25 @@
+package day017;
+
+import java.util.Scanner;
+
+public class PrintNameWithOddIndexes {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scan.nextLine();
+
+        int x = 0;
+
+        while( x < name.length() ){
+
+            if ( x % 2 == 1){
+                System.out.println(name.charAt(x));
+            }
+            ++x;
+        }
+
+    }
+}
