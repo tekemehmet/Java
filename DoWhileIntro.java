96308348d7cbbf7a3cb248b800229b26a551f425
tekemehmet <tekemehmet@yahoo.com>
2020-02-02 10:23:34 -0500

    Add files via upload

---
+++ DoWhileIntro.java
@@ -0,0 +1,33 @@
+package day018;
+
+public class DoWhileIntro {
+
+    public static void main(String[] args) {
+
+        //   do {
+        //   Take some action here and repeat it
+        //  } while (some condition is true)
+
+        int x = 1;
+
+        do {
+            System.out.println(x);
+            ++x;
+        } while (x < 10); // semicolon
+
+        System.out.println("****************************************");
+        int y = 5 ;
+        do {
+            System.out.println( y );
+            --y;
+
+        } while (y >=0 ) ;
+
+
+
+    }
+
+
+
+    }
+
