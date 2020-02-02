bac7273ad477cb4a71026d2cfba34f248ed9c33a
tekemehmet <tekemehmet@yahoo.com>
2020-02-02 10:23:59 -0500

    Add files via upload

---
+++ ForLoopIntro.java
@@ -0,0 +1,18 @@
+package day018;
+
+public class ForLoopIntro {
+
+    public static void main(String[] args) {
+
+        // count 1 to 5
+        for( int i = 1; i<=5; i++){
+            System.out.println("Happy Birthday");
+        }
+
+        for( int counter = 10; counter >= 1; counter--){
+            System.out.println("counter " + counter);
+        }
+
+
+    }
+}
