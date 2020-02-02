9559a6a15339321169edd82bb9ee9d0a9528a1c1
tekemehmet <tekemehmet@yahoo.com>
2020-02-02 10:23:21 -0500

    Add files via upload

---
+++ DoWhileAtleastRunOnce.java
@@ -0,0 +1,14 @@
+package day018;
+
+public class DoWhileAtleastRunOnce {
+
+    public static void main(String[] args) {
+
+        int x = 1000;
+
+        do{
+
+            System.out.println("Keep Going");
+        } while (x < 5 );
+    }
+}
