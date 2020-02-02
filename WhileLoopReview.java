a489622f8ac7ee020fee991ae3e8278670c6f629
tekemehmet <tekemehmet@yahoo.com>
2020-02-02 10:24:47 -0500

    Add files via upload

---
+++ WhileLoopReview.java
@@ -0,0 +1,21 @@
+package day018;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class WhileLoopReview {
+
+    public static void main(String[] args) {
+
+        int x= 10 ;
+
+        while ( x > 5) {
+            System.out.println(x + " ITS MORE THAN 5");
+            --x;
+        }
+
+        System.out.println("THE END");
+
+
+
+    }
+}
