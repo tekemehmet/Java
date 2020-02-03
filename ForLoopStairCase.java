edcbb395c486cc123719c2931ca41cf14e5dca56
tekemehmet <tekemehmet@yahoo.com>
2020-02-03 16:55:23 -0500

    Add files via upload

---
+++ ForLoopStairCase.java
@@ -0,0 +1,23 @@
+package day019;
+
+public class ForLoopStairCase {
+
+    public static void main(String[] args) {
+
+//        *
+//        * *
+//        * * *
+//        * * * *
+
+        String star = " ";
+
+        for (int i = 1; i <10 ; i++) {
+            //star = star + "* ";
+            star += "* ";
+            System.out.println(star);
+
+
+        }
+
+    }
+}
