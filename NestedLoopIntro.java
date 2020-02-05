c555993841bb30b4f3fc53912c83b9d8ce438002
tekemehmet <tekemehmet@yahoo.com>
2020-02-05 16:46:07 -0500

    Add files via upload

---
+++ NestedLoopIntro.java
@@ -0,0 +1,31 @@
+package day021;
+
+public class NestedLoopIntro {
+
+    public static void main(String[] args) {
+
+        // count from 1 to 5
+        // repeat this 10 times
+
+//        for (int x = 0; x < 5; x++) {
+//            System.out.print(x + " ");
+//        }
+//        System.out.println();
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("iteration: " + i);
+            for (int x = 1; x <= 5; x++) {
+                System.out.print(x + " ");
+
+            }
+            System.out.println();
+
+
+        }
+    }
+}
+
+
+
+
+
