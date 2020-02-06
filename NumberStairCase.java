b5f93dff14f8b20f7209a31906cfd6b3a2c96bda
tekemehmet <tekemehmet@yahoo.com>
2020-02-06 17:32:28 -0500

    Add files via upload

---
+++ NumberStairCase.java
@@ -0,0 +1,19 @@
+package day021;
+
+public class NumberStairCase {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <=10 ; i++) {
+            //System.out.println("i : " + i);
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        }
+    }
+
