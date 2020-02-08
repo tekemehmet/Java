2dd713cf1af02e7463fa16a4f198ecd16262ca17
tekemehmet <tekemehmet@yahoo.com>
2020-02-08 10:59:36 -0500

    Add files via upload

---
+++ ScannerLoopArray.java
@@ -0,0 +1,29 @@
+package day022;
+
+import java.util.Scanner;
+
+public class ScannerLoopArray {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int [] numbers = new int [10];
+
+        for (int x = 0; x < numbers.length ; x++) {
+
+            System.out.println("Enter your number " + (x+1)  );
+
+            numbers[x] = scan.nextInt();
+            System.out.println(numbers[x]  );
+
+        }
+        int sum = 0;
+        for (int x = 0; x < numbers.length ; x++) {
+            System.out.print(numbers[x] + " " );
+            sum += x;
+        }
+        System.out.println("sum = " + sum);
+
+    }
+}
