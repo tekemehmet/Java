4ca03c4fa9ae24693c27358b64503513cdfc387b
tekemehmet <tekemehmet@yahoo.com>
2020-02-07 20:08:04 -0500

    Add files via upload

---
+++ ArrayCreation_SimpleWay.java
@@ -0,0 +1,19 @@
+package day022;
+
+public class ArrayCreation_SimpleWay {
+
+    public static void main(String[] args) {
+
+//        int [] num;
+//                num = new int[] {1,2,3,4,5};
+//////////////////////////////////////////////////////////////////////////////
+
+        int[] areaCode = {703,300,312,665,954};
+
+        for (int x = 0; x < areaCode.length; x++) {
+            System.out.println("area code: " + areaCode[x]);
+
+        }
+
+    }
+}
