b5035abeb4b94d1b203b635d8a29a8423beb0c92
tekemehmet <tekemehmet@yahoo.com>
2020-02-08 10:58:49 -0500

    Add files via upload

--- LoopControlStatement.java
+++ LoopControlStatement.java
@@ -1,48 +1,60 @@
-package day021;
+package day022;
 
 public class LoopControlStatement {
 
     public static void main(String[] args) {
 
-        // break
-        // if line contains break reaches the loop will stop
+        // Store this sentence in a variable msg
+        //I struggle with Java I Like Java I love Java Everything is Awesome!
+        //loop through each and every letter
+        //1, print every other letter
+        // meaning : skip a letter each time you go through the letter
+        //	0123456789
+        //	I struggle with  -->> Isrgl ih
+        //	USE CONTINUE FOR THIS TASK
+        //2,  when you reach letter a break the loop
+        //OPTIONALLY ; BREAK WHEN YOU SEE 3RD A case insensitive
 
-        // continue
-        //if line contains break reaches the loop will skip to next iteration
+        String msg = "I struggle with Java I like Java I love Java Everything is Awesome!";
 
-        // WE CAN USE BREAK AND CONTINUE FOR ANY TYPE OF LOOP
+        for (int index = 0; index < msg.length(); index++) {
+            if (index % 2 == 1) {
+                continue;
 
-//        for (int i = 0; i <=10 ; i++) {
-//            System.out.println(i);
-//
-//        }
+            }
+            System.out.print(msg.charAt(index) + "-");
+        }
 
-        for (int i = 0; i <=10 ; i++) {
-            System.out.println(i);
-            if(i==5) {
+        // TASK 2
+
+        for (int idx = 0; idx < msg.length(); idx++) {
+
+            String currentChar = msg.substring(idx, idx + 1);
+
+            if (currentChar.equalsIgnoreCase("a")) {
                 break;
             }
+            System.out.println("currentChar = " + currentChar);
+
+        }
+        // TASK 1 and 2 Combine
+
+        for (int idx = 0; idx < msg.length(); idx++) {
+
+            if (idx % 2 == 1) {
+                continue;
+            }
+
+
+            String currentChar = msg.substring(idx, idx + 1);
+
+            if (currentChar.equalsIgnoreCase("j")) {
+                break;
+            }
+            System.out.println(idx + " index " + "currentChar = " + currentChar);
+
 
         }
 
-        int sum = 0 ;
-
-        for (int x = 0; x <=10 ; x++) {
-            System.out.println("current sum is " + sum);
-            System.out.println("If I add " + x + " dollar, it would be " + (sum+x) );
-
-            //sum = sum + x;
-
-            if (sum + x > 40) {
-                break;
-
-            } else {
-
-                sum = sum + x;
-            }
-
-
-        }
-        System.out.println("sum = " + sum);
     }
 }
