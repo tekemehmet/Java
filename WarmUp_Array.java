d5843fa182307092e303c78c082e569880d4b245
tekemehmet <tekemehmet@yahoo.com>
2020-02-10 16:35:26 -0500

    Add files via upload

--- WarmUp_Array.java
+++ WarmUp_Array.java
@@ -1,63 +1,29 @@
-package day023;
+package day024;
 
 public class WarmUp_Array {
 
     public static void main(String[] args) {
 
-        // create an int array of 7 items
-        // assign values
-        // 1.print out in reverse order
-        // 2.store last item in a variable called lastItem
-        // print it out separately
-        //3. print the item right in the middle,
-        // optionally : fin sum, max, min, average.
+        String[] groceryItem = {"apple", "banana" , "grape", "strawberry", "blueberry" , "kiwi"};
+        float[] price = {1.99f , 0.99f , 4.65f, 3.89f , 3.88f, 2.88f};
 
-        //First way to create ARRAY
-        int[] nums = new int[7];
-        nums[0] = 1;
-        nums[1] = 3;
-        nums[2] = 15;
-        nums[3] = 61;
-        nums[4] = 17;
-        nums[5] = 31;
-        nums[6] = 15;
+        for ( String eachFruit  : groceryItem ) {
 
-        // 2nd.Way to Create ARRAY
-        int[] nums2 = new int[] {11,2,54,63,1,25,9};
+            System.out.print(eachFruit + "-->");
 
-
-        // 3rd and Shortest way
-        int[] number = {5, 10, 25, 1, 23, 3, 56};
-
-
-        for (int i = number.length-1 ; i >= 0 ; i--) {  // Reverse Order the Number
-
-            System.out.println("index " + i + " : " +number[i] );
         }
+        System.out.println();
+        // print everything in one line with arrow and do not put arrow after last one
+        int itemCount = groceryItem.length;
+        int lastItemIndex= itemCount-1;
+        String lastFruit = groceryItem[lastItemIndex]; // groceryItem[groceryItem.length-1]
 
-        int arraySize = number.length;
-        int lastItemIndex = arraySize-1;
+        for ( String eachFruit  : groceryItem ) {
 
-        int lastItem = number[lastItemIndex];
-        System.out.println("last item Value = " + lastItem);
-
-        System.out.println("Middle value is: " + number[number.length/2]); // middle value
-
-        int counter = 0;
-        int sum = 0;
-        for (int x = 0; x <= lastItemIndex; x++) {
-
-            sum = sum + number[x];
-            counter++;
+            System.out.print(eachFruit);
+            if (! eachFruit.equals(lastFruit))
+                System.out.print("-->");
         }
-        System.out.println("Sum: " + sum);
-        System.out.println("counter = " + counter);
-        System.out.println("Average Value: " + 123/7);
-
-
-
-
-
 
     }
 }
