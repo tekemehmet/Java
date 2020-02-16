d317010b08cb30ab49a2bba6967eede9b75a2f6b
tekemehmet <tekemehmet@yahoo.com>
2020-02-16 07:38:19 -0500

    Add files via upload

---
+++ MethodPractice.java
@@ -0,0 +1,33 @@
+package day031;
+
+public class MethodPractice {
+
+    public static void main(String[] args) {
+        Method_Task.sayHello();
+        calcuate();
+    }
+
+    public static void calcuate() {
+
+         int num1 = 5;
+        double num2 = 3.0d;
+        float num3 = 3.0f;
+
+        double result1 = num1 + num2;
+        double result2 = num1 - num2;
+        double result3 = num1 * num2;
+        double result4 = num1 / num2;
+        float result5 = num1 / num3;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+
+
+
+    }
+
+
+}
