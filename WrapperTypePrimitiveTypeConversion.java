6203670b80c6c1890421b2c8004e60d43d43f527
tekemehmet <tekemehmet@yahoo.com>
2020-02-21 19:53:22 -0500

    Add files via upload

---
+++ WrapperTypePrimitiveTypeConversion.java
@@ -0,0 +1,62 @@
+package day035;
+
+public class WrapperTypePrimitiveTypeConversion {
+
+    public static void main(String[] args) {
+
+        Integer num1 = 100 ; // auto-boxing 100---> new Integer(100)
+
+        int num2 = Integer.valueOf("200"); // auto-unboxing into 200
+
+        // int num3 = "200"; // Compile Error
+
+        // This is how we can use parseInt to convert String to int
+        int num3 = Integer.parseInt("200");
+        /*
+        * Integer.parseInt("numberHere")
+        *   It return an int value converted from String
+        *
+        * Integer.valueOf("numberHere")
+        *   it return an Integer Object by wrapping up the number
+        *
+        * YOU MAY USE WHICHEVER YOU WANT,BECAUSE :
+        * Auto-boxing and Auto-unboxing will take care of the conversion anyway
+         */
+
+        // Use this number IPR2012-00001 and store the year into a int variable
+        // This case number always start with 3 character either IPR , PGR , CBM , DER
+        // follwed by year, follwed by dash and 5 digit number
+        String caseNumber = "IPR2012-00001" ;
+        int year; // get 2012 out of it
+
+        String strYear = caseNumber.substring(3,7);
+        year = Integer.parseInt(strYear);
+        System.out.println("strYear = " + strYear);
+
+
+
+        System.out.println(getYearOutOfCaseNumber("FER2019-00035"));
+        System.out.println(getYearOutOfCaseNumber("IPR2012-00001"));
+        System.out.println(getYearOutOfCaseNumber("CBM2001-00051"));
+
+
+    }
+
+    public static int getYearOutOfCaseNumber(String caseNumber){
+
+        // do validation on case number
+        // TODO : Case number should always start with either
+        // CBM , IPR, PGR , DER
+        // FOLLOWED BY 4 DIGIT NUMBER
+        // FOLLOWED BY -
+        // AND FOLLOWED BY 5 DIGIT
+
+//        int year; // get 2012 out of it
+//        String strYear = caseNumber.substring(3,7);
+//        year = Integer.parseInt(strYear);
+//        return year;
+
+        return Integer.parseInt(caseNumber.substring(3,7) );
+
+    }
+}
