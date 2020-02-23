81e5e46cf7595ce76ab8548980dfd0e095c8f964
tekemehmet <tekemehmet@yahoo.com>
2020-02-23 15:06:28 -0500

    Add files via upload

---
+++ WrapperREview.java
@@ -0,0 +1,13 @@
+package day036;
+
+public class WrapperREview {
+
+    public static void main(String[] args) {
+
+        Integer i1 = new Integer(1000000000);
+        Integer i2 = Integer.valueOf(i1);
+
+        Integer i3 = 45 ;
+
+    }
+}
